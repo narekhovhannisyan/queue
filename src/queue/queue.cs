444655082314c7d2622c2f015afce7870d05c3fd
narekhovhannisyan <narekgilmour@gmail.com>
2017-10-15 17:53:14 +0400

    src: added queue cs file, added class lQueue with class node and methods
     - added lQueue class wich have class Node, methods enqueue, logFromTail
     - added public method enqueue wich adds new node to existing queue, if queue not exists, it creates first node
     - added logFromTail funciton wich logs current queue
     - in Main tested existing functions

---
+++ src/queue/queue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace queue_namespace
+{
+  class lQueue
+  {
+    class Node
+    {
+      public Node Next;
+      public Node Prev;
+      public int Value;
+    }
+
+    Node head, tail, current = null;
+
+    public void enqueue(int value)
+    {
+      Node newNode = new Node();
+      if (head == tail)
+      {
+        newNode.Next = head;
+        newNode.Value = value;
+        head = current = newNode;
+      }
+      else
+      {
+        current = (tail == null) ? head : tail;
+        newNode.Next = current;
+        newNode.Value = value;
+        tail = newNode;
+        current.Prev = tail;
+      }
+    }
+
+    public void logFromTail()
+    {
+      while (tail != null)
+      {
+        Console.WriteLine(tail.Value);
+        tail = (tail.Next != null) ? tail.Next : null;
+      }
+    }
+  }
+  class queue
+  {
+    static void Main(string[] args)
+    {
+      lQueue queue = new lQueue();
+      queue.enqueue(5);
+      queue.enqueue(6);
+      queue.enqueue(7);
+      queue.enqueue(87);
+      queue.enqueue(23);
+      queue.enqueue(2);
+      queue.enqueue(0);
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.logFromTail();
+    }
+  }
+}
